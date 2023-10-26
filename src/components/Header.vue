afb92b5214f50dfa95a533487c7a75f9271b6d1a
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-10-26 19:37:30 +0900

    Update Header.vue
    header변경

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-8 clo-md-7 py-4">
-                        <h4 class="text-white">봉사 신청 사이트</h4>
+                        <h4 class="text-white">IT Volunteer Web</h4>
                         <p class="text-muted">회원님의 관심 분야 혹은 전공에 맞추어 IT 교육 봉사를 신청 가능하게 하여 봉사자와 학습자 모두 만족 가능한 사이트 입니다.</p>
                     </div>
                     <div class="col-sm-4 py-4">
@@ -13,11 +13,11 @@
                             <li>
                                 <router-link to="/" class="text-white">메인 화면</router-link>
                             </li>
-                            <li>
-                                <router-link to="/phome" class="text-white">포인트 상점</router-link>
+                            <li v-if="$store.state.account.id">
+                                <router-link to="/orders" class="text-white">봉사 신청 기록</router-link>
                             </li>
                             <li v-if="$store.state.account.id">
-                                <router-link to="/orders" class="text-white">내 정보</router-link>
+                                <router-link to="/porders" class="text-white">포인트물품 구매 내역</router-link>
                             </li>
                             <li>
                                 <router-link to="/createAccount" class="text-white">회원 가입</router-link>
@@ -28,8 +28,12 @@
                                 <a to="/login" class="text-white" @click="logout()" v-else>로그아웃</a>
                             </li>
                             <li>
-                                <router-link to="/Read" class="text-white">게시판</router-link>
+                              <router-link to="/edit" class="text-white">관리자 페이지</router-link>
                             </li>
+                             <li>
+                                <router-link to="/Crawling" class="text-white">1365 크롤링</router-link>
+                             </li>
+
                         </ul>
                     </div>
                 </div>
@@ -44,14 +48,20 @@
                         <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
                         <circle cx="12" cy="13" r="4"/>
                     </svg>
-                    <strong>IT 봉사 웹</strong>
+                    <strong>ITEER</strong>
                 </router-link>
-                <router-link to="/cart" class="cart btn" v-if="$store.state.account.id">
-                    <i class="fa fa-heart" aria-hidden="true"></i>
+                  <router-link to="/phome" class="cart btn">
+                    <i class="fa fa-home" aria-hidden="true"></i> 포인트 상점
                 </router-link>
-                <router-link to="/pcart" class="pcart btn" v-if="$store.state.account.id">
-                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                </router-link>
+              <router-link to="/cart" class="cart btn" v-if="$store.state.account.id">
+                <i class="fa fa-heart" aria-hidden="true"></i> 봉사장소 관심목록
+              </router-link>
+              <router-link to="/pcart" class="cart btn" v-if="$store.state.account.id">
+                <i class="fa fa-shopping-cart" aria-hidden="true"></i> 포인트 물품 장바구니
+              </router-link>
+              <router-link to="/Read" class="pcart btn">
+                <i class="fa fa-list-alt" aria-hidden="true"></i> 게시판
+              </router-link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
                         aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
