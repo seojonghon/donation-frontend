53208c1152b37716eb51e0b48794ac96a7e5637c
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-10-26 19:50:23 +0900

    Update router.js
    라우팅 수정

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -10,28 +10,34 @@ import Create from "@/pages/Create.vue";
 import Detail from "@/pages/Detail.vue";
 import BoardForm from "@/pages/BoardForm.vue";
 import Inform from "@/pages/Inform.vue";
+import POrder from "@/pages/POrder.vue";
+import POrders from "@/pages/POrders.vue";
 import PHome from "@/pages/PHome.vue";
 import PCart from "@/pages/PCart.vue";
-import POrders from "@/pages/POrders.vue";
-import POrder from "@/pages/POrder.vue";
+import Boards from "@/pages/Message.vue";
+import Crawler from "@/pages/Crawler.vue";
+import Message from "@/pages/Message.vue"; // 추가
+
 
 
 const routes= [
     {path: '/', component: Home},
     {path: '/login', component: Login},
     {path: '/cart', component: Cart},
-    {path: '/pcart', component: PCart},
     {path: '/order', component: Order},
     {path: '/orders', component: Orders},
+    {path: "/Read", name: "Read", component: Read},
+    {path: '/create/:contentId?', name: 'Create', component: Create},
+    {path: "/detail/:contentId", name: "Detail", component: Detail},
+    {path: "/createAccount", name: "BoardForm", component: BoardForm},
+    {path: "/Inform", name: "Inform", component: Inform},
     {path: '/porder', component: POrder},
     {path: '/porders', component: POrders},
-    {path: "/Read", name: "Read", component: Read},
-     {path: "/create", name: "Create", component: Create},
-    {path: "/inform", name: "Inform", component: Inform},
-    {path: "/createAccount", name: "BoardForm", component: BoardForm},
-    //{path: '/create/:contentId?', name: 'Create', component: Create},
-    {path: "/detail/:contentId", name: "Detail", component: Detail},
-    {path: '/phome', component: PHome}
+    {path: '/phome', component: PHome},
+    {path: '/pcart', component: PCart},
+    {path: '/boards', component: Boards},
+    {path: '/edit', component: Message},
+    {path: '/Crawling', component: Crawler} // 추가
 ]
 
 const router = createRouter({
